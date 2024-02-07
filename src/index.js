792916b5c307fa8054693000d089ccdb36568c0e
lunaticriz <mdrizwan0524@gmail.com>
2024-02-07 12:04:52 +0530

    add code in app.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import dotenv from 'dotenv';
 import connect from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: './env'
 });
 
-connect();
+connect()
+.then(() => {
+    app.on('error', (err) => {
+        console.error(err);
+        throw err;
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.info(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((err) => { 
+    console.error("Mongodb connection failed err" + err);
+});
