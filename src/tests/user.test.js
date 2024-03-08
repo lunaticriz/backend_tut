dccc7208f39b82261694c0608b9d4cea25e419c8
lunaticriz <mdrizwan0524@gmail.com>
2024-03-08 16:39:00 +0530

    logout unit test added

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -56,26 +56,72 @@ after(function (done) {
 //   });
 // });
 
-describe("POST /user/login", () => {
-  it("should return status 400 for invalid credentials", async () => {
+// describe("POST /user/login", () => {
+//   it("should return status 400 for invalid credentials", async () => {
+//     const response = await request(app)
+//       .post("/api/v1/users/login")
+//       .send({ email: "invaliduser", password: "invalidpassword" });
+//     expect(response.status).to.equal(400);
+//   });
+
+//   it("should return status 200 and user details for valid credentials", async () => {
+//     const response = await request(app)
+//       .post("/api/v1/users/login")
+//       .send({ email: "johndoe@john.com", password: "JohnDoe" });
+//     console.log(response._body);
+//     expect(response.status).to.equal(200);
+//     expect(response._body).to.have.property("statusCode", 200);
+//     expect(response._body.message).to.equal("User logged in successfully");
+//     expect(response._body.data).to.be.an("object");
+//     expect(response._body.data.user).to.have.property("_id");
+//     expect(response._body.data.user).to.have.property(
+//       "email",
+//       "johndoe@john.com"
+//     );
+//   });
+// });
+
+// describe("GET POST /user/current-user", () => {
+//   it("should be return failed to fetch current user details", async () => {
+//     const response = await request(app)
+//       .get("/api/v1/users/current-user")
+//       .set("Authorization", "")
+//       .set("Content-Type", "application/json");
+//     expect(response.status).to.equal(401);
+//   });
+//   it("should be return current user details", async () => {
+//     const response = await request(app)
+//       .get("/api/v1/users/current-user")
+//       .set(
+//         "Authorization",
+//         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWU4Mzk4YzIwYWE0NGY2ZTRkNDJlMDgiLCJlbWFpbCI6ImpvaG5kb2VAam9obi5jb20iLCJ1c2VyTmFtZSI6ImpvaG5kb2UiLCJmdWxsTmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzA5ODkyMjAxLCJleHAiOjE3MDk5Nzg2MDF9.QOWVHsHXfXXd-4dxCqvXlbAApD_zE8q0hCJO9_kz-jU"
+//       )
+//       .set("Content-Type", "application/json");
+//     expect(response.status).to.equal(200);
+//     expect(response._body).to.have.property("statusCode", 200);
+//     expect(response._body.data).to.be.an("object");
+//     expect(response._body.message).to.equal("User retrieved successfully");
+//   });
+// });
+
+describe("POST /user/logout", () => {
+  it("should not be logout the user from the application", async () => {
     const response = await request(app)
-      .post("/api/v1/users/login")
-      .send({ email: "invaliduser", password: "invalidpassword" });
-    expect(response.status).to.equal(400);
+      .post("/api/v1/users/logout")
+      .set("Authorization", "")
+      .set("Content-Type", "application/json");
+    expect(response.status).to.equal(401);
   });
 
-  it("should return status 200 and user details for valid credentials", async () => {
+  it("should be logout the user from the application", async () => {
     const response = await request(app)
-      .post("/api/v1/users/login")
-      .send({ email: "johndoe@john.com", password: "JohnDoe" });
+      .post("/api/v1/users/logout")
+      .set(
+        "Authorization",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWU4Mzk4YzIwYWE0NGY2ZTRkNDJlMDgiLCJlbWFpbCI6ImpvaG5kb2VAam9obi5jb20iLCJ1c2VyTmFtZSI6ImpvaG5kb2UiLCJmdWxsTmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNzA5ODkzNzM3LCJleHAiOjE3MDk5ODAxMzd9.hUopDwT3OG8ryRvhIp1nkZhCx5UvihdKlbQkXWAXjr8"
+      )
+      .set("Content-Type", "application/json");
     expect(response.status).to.equal(200);
-    expect(response._body).to.have.property("statusCode", 200);
-    expect(response._body.message).to.equal("User logged in successfully");
-    expect(response._body.data).to.be.an("object");
-    expect(response._body.data.user).to.have.property("_id");
-    expect(response._body.data.user).to.have.property(
-      "email",
-      "johndoe@john.com"
-    );
+    expect(response._body.message).to.equal("User logged out successfully");
   });
 });
